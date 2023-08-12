1eafaf22acf62d8359da27a2d4ff0d3b56f2d377
izouki-collab <abdelkrim.izoukki@gmail.com>
2023-08-12 08:01:50 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,19 @@
  int main(void)
 {
 	char c;
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
+
+	for(c = 'a'; c <= 'z'; c++)
 	{
-        for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
 	}
-	        putchar(c);
+
+        for(c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
         }
+
          putchar('\'n);
+
 	 return(0);
 
 }
