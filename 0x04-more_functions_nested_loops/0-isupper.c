f692df5875ed63c6e0f75021fae7788a561bb031
izouki-collab <abdelkrim.izoukki@gmail.com>
2023-08-18 09:40:34 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * function that checks for uppercase character
+ * return: always (success)
+ *
+ */
+ int _isupper(int c);
+	int main (c);{ 
+	    c = 'A';
+    	    printf("%c: %d\n", c, _isupper(c));
+    	    c = 'a';
+	    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
+
