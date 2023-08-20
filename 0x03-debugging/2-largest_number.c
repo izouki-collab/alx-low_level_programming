a7cba91b410bb7e2760104c49f09b9cb8991ee85
izouki-collab <abdelkrim.izoukki@gmail.com>
2023-08-20 04:50:56 -0700

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,7 +10,7 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
+
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -27,7 +27,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-
-return(largest);
+	return (largest);
 }
 
