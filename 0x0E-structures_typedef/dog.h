250910197680d3a9eda04a668e332e1092eda5d3
izouki-collab <abdelkrim.izoukki@gmail.com>
2023-09-13 14:51:04 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-
 /**
- * struct dog: a dog struct
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
- * description: just a long dog struct in a betty world
- */
-
+* struct dog - dog structure
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif
