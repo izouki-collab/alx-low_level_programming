0377ddd292cf35d4690b256308e445fa65998af6
izouki-collab <abdelkrim.izoukki@gmail.com>
2023-09-13 13:11:04 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this readme is about structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog: a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * description: just a long dog struct in a betty world
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
