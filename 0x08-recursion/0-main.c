8d5e551d9274af834fa5c78a7f668074fdc0e046
izouki-collab <abdelkrim.izoukki@gmail.com>
2023-10-26 05:21:00 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-    _puts_recursion("Puts with recursion");
-    return (0);
+	_puts_recursion("Puts with recursion");
+	return (0);
 }
--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _puts_recursion - Prints a string, followed by a new line.
  * @s: the string to be printed
@@ -5,11 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s[0] != '\0')
+	if (*s != '\0')
 	{
-		_putchar(s[0]);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	else
+	if (*s == '\0')
+	{
 		_putchar('\n');
+	}	
 }
